bfef2395ca26277e31682657d3ec3c1fa338c7bf
Natalya Posokhova <53231727+NatalyaPosokhova@users.noreply.github.com>
2024-10-13 22:19:24 +0500

    Добавила тесты на GetPriorityOperationExpression

--- Calculator.UnitTests/ParserTests.cs
+++ Calculator.UnitTests/ParserTests.cs
@@ -25,5 +25,22 @@ namespace Calculator.UnitTests
 			Assert.That(act.StartIndex, Is.EqualTo(startIndex));
 			Assert.That(act.EndIndex, Is.EqualTo(endIndex));
 		}
+
+
+
+		[TestCase("25+2-3*10-2-3/28", 6, 5, 8)]
+		[TestCase("25+10-2", 2, 0, 4)]
+		[TestCase("25/10-2*8", 2, 0, 4)]
+		[TestCase("25", -1, 0, 1)]
+		public void TryGetPriorityOperationExpression_Success(string exp, int priorityOpIndex, int startIndex, int endIndex)
+		{
+			//Arrange
+			//Act
+			var act = _parser.GetPriorityOperationExpression(exp, priorityOpIndex);
+
+			//Assert
+			Assert.That(act.StartIndex, Is.EqualTo(startIndex));
+			Assert.That(act.EndIndex, Is.EqualTo(endIndex));
+		}
 	}
 }
