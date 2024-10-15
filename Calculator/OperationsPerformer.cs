f360f32df0aaf15a32adc57fa3e6997d3ac6ceee
Natalya Posokhova <53231727+NatalyaPosokhova@users.noreply.github.com>
2024-10-15 09:46:28 +0500

    Добавила реализацию OperationsPerformer

--- Calculator/OperationsPerformer.cs
+++ Calculator/OperationsPerformer.cs
@@ -6,22 +6,22 @@ namespace Calculator
 	{
 		public decimal Add(decimal a, decimal b)
 		{
-			throw new NotImplementedException();
+			return a + b;
 		}
 
 		public decimal Divide(decimal a, decimal b)
 		{
-			throw new NotImplementedException();
+			return a / b;
 		}
 
 		public decimal Multiply(decimal a, decimal b)
 		{
-			throw new NotImplementedException();
+			return a * b;
 		}
 
 		public decimal Substract(decimal a, decimal b)
 		{
-			throw new NotImplementedException();
+			return a - b;
 		}
 	}
 }
