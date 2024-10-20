ab9614e8c203fd5059c85a199a39a932f1da3434
Natalya Posokhova <53231727+NatalyaPosokhova@users.noreply.github.com>
2024-10-20 22:24:38 +0500

    Валидируем с пом-ю рефлексии

--- Calculator/Checkers/AllowedSymbolsChecker.cs
+++ Calculator/Checkers/AllowedSymbolsChecker.cs
@@ -1,17 +1,32 @@
-﻿using System.Text.RegularExpressions;
+﻿using Calculator.Interfaces;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Calculator.Checkers
 {
 	internal class AllowedSymbolsChecker : Checker
-	{		
-		public AllowedSymbolsChecker(Checker Next) : base(Next)	{	}
+	{
+		public AllowedSymbolsChecker(Checker Next) : base(Next) { }
 		public override bool ValidateString(string exp)
 		{
-			if (!exp.All(ch => Regex.IsMatch(ch.ToString(), Constants.AllowedSymbols)))
+			var sb = new StringBuilder(Constants.AllowedSymbols);
+
+			var types = AppDomain.CurrentDomain.GetAssemblies()
+				.SelectMany(s => s.GetTypes())
+				.Where(p => typeof(IOperation).IsAssignableFrom(p) && !p.IsInterface);
+
+			foreach (var type in types)
+			{
+				var obj = (IOperation)Activator.CreateInstance(type);
+				sb.Append('|');
+				sb.Append('\\');
+				sb.Append(obj.Symbol);
+			}
+
+			if (!exp.All(ch => Regex.IsMatch(ch.ToString(), sb.ToString())))
 				return false;
 
 			return base.ValidateString(exp);
 		}
-
 	}
 }
