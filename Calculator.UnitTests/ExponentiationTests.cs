d033f7619023930274d506a885ffb21546b02c0c
Natalya Posokhova <53231727+NatalyaPosokhova@users.noreply.github.com>
2024-10-21 00:56:04 +0500

    Добавила операцию возведения в степень без реализации + тесты

---
+++ Calculator.UnitTests/ExponentiationTests.cs
@@ -0,0 +1,33 @@
+﻿using Calculator.Interfaces;
+using Calculator.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.UnitTests
+{
+	public class ExponentiationTests
+	{
+		private IOperation _exponentiationOperation;
+
+		[SetUp]
+		public void Setup()
+		{
+			_exponentiationOperation = new Exponentiation();
+		}
+
+		[TestCase(-2, 3, -8)]
+		[TestCase(7.1, 2, 50.41)]
+		public void TryExponentiation_Success(decimal a, decimal b, decimal expected)
+		{
+			//Arrange
+			//Act
+			var act = _exponentiationOperation.Execute(a, b);
+
+			//Assert
+			Assert.That(act, Is.EqualTo(expected));
+		}
+	}
+}
---
+++ Calculator/Operations/Exponentiation.cs
@@ -0,0 +1,13 @@
+﻿using Calculator.Interfaces;
+
+namespace Calculator.Operations
+{
+	public class Exponentiation : IOperation
+	{
+		public char Symbol => '^';
+
+		public int Weight => 5;
+
+		public decimal Execute(decimal a, decimal b) => throw new NotImplementedException();
+	}
+}
